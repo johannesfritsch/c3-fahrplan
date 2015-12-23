fd8aa6970f47ecfb3e332e5615520ea75be30465
Johannes Fritsch <j@jfritsch.de>
2015-12-23 19:54:09 +0100

    Added test for hasCongressBegun

--- test/modules/has-congress-begun.test.js
+++ test/modules/has-congress-begun.test.js
@@ -3,8 +3,7 @@ var sinon      = require('sinon');
 var proxyquire = require('proxyquire');
 
 describe('Fahrplan', function(){
-	it('returns if the congress has begun', function(done){
-		
+	it('hasCongressBegun returns true during congress', function(done){
 		var clock = sinon.useFakeTimers(new Date(2015, 12-1, 28).getTime());
 
 		hcb = proxyquire('../../modules/has-congress-begun', { 
@@ -23,6 +22,30 @@ describe('Fahrplan', function(){
 		hcb(function(err, hasBegun){
 			assert(err == null);
 			assert(hasBegun == true);
+			clock.restore();
+			done();
+		});
+	});
+
+	it('hasCongressBegun returns false during before', function(done){
+		var clock = sinon.useFakeTimers(new Date(2015, 12-1, 10).getTime());
+
+		hcb = proxyquire('../../modules/has-congress-begun', { 
+			'../utils/downloader': function(cb){
+				cb(null, { 
+					schedule: {
+						conference: {
+							start: '2015-12-27',
+							end: '2015-12-30'
+						}
+					}
+				});
+			}
+		});
+
+		hcb(function(err, hasBegun){
+			assert(err == null);
+			assert(hasBegun == false);
 			clock.restore();
 			done();
 		});
